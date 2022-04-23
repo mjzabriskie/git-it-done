e1c1a6039883d834019065cc3b94095eb5912d81
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-04-22 20:19:20 -0600

    added error handling on homepage and single js files

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -16,9 +17,26 @@ var getRepoIssues = function(repo) {
                 }
             });
         }else{
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
+}
+
+var getRepoName = function(){
+    //grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        //display repo name on page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
 }
 
 var displayIssues = function(issues){
@@ -64,10 +82,12 @@ var displayWarning = function(repo){
     //add text to warning container
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
     var linkEl = document.createElement("a");
-    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.textContent = "GitHub.com";
     linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
     linkEl.setAttribute("target", "_blank");
     limitWarningEl.appendChild(linkEl);
 }
 
-getRepoIssues("facebook/react");
+
+//getRepoIssues();
+getRepoName();
